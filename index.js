7c66d09bd90366deff87aeda06e105b953f59569
HarshSoni69 <rajeevsoni1972@gmail.com>
2024-07-26 18:50:50 +0530

    More changes to the config file5

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 require('dotenv').config();
-const connectDB = require('./config/database')
+const connectDB = require('./config/database');
 const express = require('express');
 const ProductRouter = require('./routes/product');
-const path=require('path');
-const cors=require('cors');
+const path = require('path');
+const cors = require('cors');
 
 const server = express();
 
 // Middleware to parse JSON bodies
 server.use(express.json());
 server.use(cors());
-server.use( '/api',ProductRouter);
-server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
-server.use('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,process.env.PUBLIC_DIR,'index.html'));
-})
 
-//connect to db
+// API routes
+server.use('/api', ProductRouter);
+
+// Serve static files from the build directory
+const buildPath = path.resolve(__dirname, 'build');
+server.use(express.static(buildPath));
+
+// Serve React app for all other routes
+server.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+// Connect to database
 connectDB();
 
-// Use the router with the specified mount path
-
-server.listen(process.env.PORT, () => {
-  console.log("Server is running on http://localhost:8080");
+// Start the server
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
